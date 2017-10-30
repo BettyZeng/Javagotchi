809911b4bb4f802a36473138ca48b1d45198318d
BettyZeng <33221205+BettyZeng@users.noreply.github.com>
2017-10-30 12:30:02 +0000

    Update Game.java

--- src/Game.java
+++ src/Game.java
@@ -4,84 +4,63 @@ package PACKAGE_NAME;
  * Created by k1763913 on 30/10/17.
  */
 public class Game {
-int energy,fullness,happiness;
-    String name;
-    public Creature(String name){
-        energy = 15;
-        fullness = 15;
-        happiness = 15;
+ Scanner scanner = new Scanner(System.in);
+    Creature creature;
+    boolean userQuits = false;
 
-        this.name = name;
-    }
-    public void eat(){
-        if(fullness >= 12) {
-            print("Woops," + this.getName() + "is already full.");
-        }else {
-            fullness += 8;
-            happiness -= 6;
-            energy -= 4;
-            if (fullness > 15) {
-                fullness = 15;
-            }
-            print("Yum," + this.getName() + "really enjoy that!");
+
+    public void run(){
+        boolean userQuits = false;
+        startTutorial();
+        while (!userQuits){
+            interact();
+            print(this.creature.checkStatus());
         }
     }
+    private void startTutorial(){
+        print("Hi there! Welcome to Javagotchi.");
+        print("What's the name of your Creature?");
+
+        String name = scanner.nextLine();
+
+        this.creature = new Creature(name);
 
 
-    public void sleep(){
-        if(energy >= 12) {
-            print("Woops," + this.getName() + "is not that sleepy.");
-        }else {
-            energy += 8;
-            happiness -= 6;
-            fullness -= 4;
-            if (energy > 15) {
-                energy = 15;
-            }
-        }
-    }
+        print("Great,you can now meet" + this.creature.getName() + "!");
 
-    public void play(){
-
-
-        happiness += 8;
-        energy -= 6;
-        fullness -= 4;
-
-        print("This was so much fu! Let's do it again!");
-
-
-    }
-    public String checkStatus(){
-        String status = "";
-        if (happiness <= 6){
-            status += "Oh," + this.getName() +" is feeling sad!\n" ;
         }
 
-        if (energy <= 6) {
-            status += "Hmm," + this.getName() + " is really tried!\n";
+
+
+    private  void interact(){
+        print("What would you like to do?");
+        print("1.PLay");
+        print("2.Eat");
+        print("3.Sleep");
+        print("4.Quit");
+
+        int choice =  scanner.nextInt();
+
+        switch (choice){
+            case 1:
+                creature.play();
+                break;
+            case 2:
+                creature.eat();
+                break;
+            case 3:
+                creature.sleep();
+                break;
+            case 4:
+                userQuits = true;
+                break;
+            default:
+                print("Woops,you have to pick a number between 1 and 4");
+                break;
+
         }
-        if (fullness <= 6){
-            status += "Woops," + this.getName() +" is super hungry!\n" ;
-        }
-
-        if (happiness <= 0 || energy <= 0 || fullness <= 0 );{
-            print("Oh no!" + this.getName() + "has died. Your are a terrible owner!");
-            System.exit(0);
-        }
-        return status;
-
-
-
-    }
-
-    public String getName() {
-        return name;
     }
     public void print(String s){
         System.out.println(s);
     }
-}
-
-
 }
